c3185bc400dc0072a53571765435c0294adf4f73
Geórgia Maria <georgiamcs@gmail.com>
2023-02-15 00:30:20 -0300

    CrudCompra - Carregando lookups de fornecedor e vacina

--- projetoConclusaoPos/vacine/vacine-frontend/src/app/components/crud-com-lookup/crud-com-lookup.component.ts
+++ projetoConclusaoPos/vacine/vacine-frontend/src/app/components/crud-com-lookup/crud-com-lookup.component.ts
@@ -1,11 +1,9 @@
-import { Subscription } from 'rxjs';
-import { CrudService } from './../../shared/services/crud/crud.service';
 import { Component } from '@angular/core';
 import { CrudComponent } from 'src/app/components/crud/crud.component';
-import { EntityModel } from 'src/app/shared/models/entity.model';
-import { EntityNomeModel } from './../../shared/models/entity-nome.model';
 import { MensagemFeedback } from 'src/app/shared/classes/mensagem-feedback.class';
 import { TipoMensagemFeedback } from 'src/app/shared/enums/tipo-mensagem-feedback.enum';
+import { EntityModel } from 'src/app/shared/models/entity.model';
+import { EntityNomeModel } from './../../shared/models/entity-nome.model';
 
 @Component({
   selector: 'vacine-crud-com-lookup',
@@ -18,10 +16,7 @@ export class CrudComLookupComponent<
   protected isCarregando: boolean = false;
   protected erroCarregando: boolean = false;
 
-  protected filtrarValorLista(
-    lista: EntityNomeModel[],
-    nome: string
-  ): any[] {
+  protected filtrarValorLista(lista: EntityNomeModel[], nome: string): any[] {
     const filterValue = nome.toLowerCase();
     return lista.filter((i) => i.nome.toLowerCase().includes(filterValue));
   }
@@ -30,26 +25,33 @@ export class CrudComLookupComponent<
     return entity?.nome ? entity.nome : '';
   }
 
-  protected preencherListaLookup(
-    servicoRecuperarLista: CrudService<EntityModel>,
-    listaLookup: EntityModel[]
-  ): Subscription {
-    return servicoRecuperarLista.listar().subscribe({
-      next: (lista) => {
-        listaLookup = lista;
-        console.log('listaLookup', listaLookup);
-      },
-      error: (e) => {
-        this.erroCarregando = true;
-        this.isCarregando = false;
-        this.addMensagem(
-          new MensagemFeedback(
-            TipoMensagemFeedback.ERRO,
-            'Erro ao recuperar fornecedores!'
-          )
-        );
-        throw 'Erro ao recuperar fornecedores! Detalhes: ' + e;
-      },
-    });
+  protected tratarErroCarregarLookup(erro: any, nomeCampo: string) {
+    this.erroCarregando = true;
+    this.isCarregando = false;
+    this.addMensagem(
+      new MensagemFeedback(
+        TipoMensagemFeedback.ERRO,
+        `Erro ao recuperar lista de! ${nomeCampo}. Detalhe do erro: ${erro}`
+      )
+    );
+  }
+
+  protected filtrarPeloValor(
+    lista: any[],
+    valor: any,
+    nomeFormControl: string
+  ): any[] {
+    let ehString = typeof valor === 'string';
+    const nome = typeof valor === 'string' ? valor : valor?.nome;
+
+    if (ehString && nome && nome != '' && lista && lista.length > 0) {
+      let item = lista.find(
+        (item) => item.nome.toLowerCase() == nome.toLowerCase()
+      );
+      if (item) {
+        this.form.get(nomeFormControl)!.patchValue(item, { emitEvent: false });
+      }
+    }
+    return nome ? this.filtrarValorLista(lista, nome as string) : lista.slice();
   }
 }
--- projetoConclusaoPos/vacine/vacine-frontend/src/app/pages/entidades/compra/crud-compra/crud-compra.component.ts
+++ projetoConclusaoPos/vacine/vacine-frontend/src/app/pages/entidades/compra/crud-compra/crud-compra.component.ts
@@ -1,14 +1,13 @@
-import { CrudComLookupComponent } from 'src/app/components/crud-com-lookup/crud-com-lookup.component';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, Observable, startWith } from 'rxjs';
-import { MensagemFeedback } from 'src/app/shared/classes/mensagem-feedback.class';
-import { CrudComponent } from './../../../../components/crud/crud.component';
+import { CrudComLookupComponent } from 'src/app/components/crud-com-lookup/crud-com-lookup.component';
+import { VacinaService } from 'src/app/services/vacina/vacina.service';
 import { FornecedorService } from './../../../../services/fornecedor/fornecedor.service';
-import { TipoMensagemFeedback } from './../../../../shared/enums/tipo-mensagem-feedback.enum';
 import { Fornecedor } from './../../../../shared/models/fornecedor.model';
+import { Vacina } from './../../../../shared/models/vacina.model';
 
 @Component({
   selector: 'vacine-crud-compra',
@@ -19,12 +18,19 @@ export class CrudCompraComponent
   extends CrudComLookupComponent<Fornecedor>
   implements OnInit
 {
+  protected readonly nomeControlFornecedor = 'fornecedor';
+  protected readonly nomeControlVacina = 'vacina';
+
   //Filtro de fornecedores
   fornecedores: Fornecedor[] = [];
   fornecedoresFiltrados!: Observable<Fornecedor[]>;
 
+  vacinas: Vacina[] = [];
+  vacinasFiltradas!: Observable<Fornecedor[]>;
+
   constructor(
     private _service: FornecedorService,
+    private serviceVacina: VacinaService,
     private _formBuilder: FormBuilder,
     private _router: Router,
     private _activatedRoute: ActivatedRoute,
@@ -36,31 +42,8 @@ export class CrudCompraComponent
     this.definirIdentificadoresEntidade();
     this.preencherAtributosGenericosCrud();
   }
-
   override ngOnInit(): void {
     super.ngOnInit();
-
-    this.preencherLookupFornecedor();
-  }
-
-  private preencherLookupFornecedor() {
-    this.subscription = this.service.listar().subscribe({
-      next: (lista) => {
-        this.fornecedores = lista;
-        console.log(lista);
-      },
-      error: (e) => {
-        this.erroCarregando = true;
-        this.isCarregando = false;
-        this.addMensagem(
-          new MensagemFeedback(
-            TipoMensagemFeedback.ERRO,
-            'Erro ao recuperar fornecedores!'
-          )
-        );
-        throw 'Erro ao recuperar fornecedores! Detalhes: ' + e;
-      },
-    });
   }
 
   private definirAtributosInjetores() {
@@ -81,35 +64,75 @@ export class CrudCompraComponent
   protected override buildForm() {
     this.form = this.formBuilder.group({
       fornecedor: [null],
+      vacina: [null],
     });
 
+    this.preencherCamposLookup();
+    this.setChangeFornecedorParaFiltrarValores();
+    this.setChangeVacinaParaFiltrarValores();
+  }
+
+  private preencherCamposLookup() {
+    this.setLookupFornecedor();
+    this.setLookupVacina();
+  }
+
+  private setLookupFornecedor() {
+    this.subscription = this.service.listar().subscribe({
+      next: (listaFornecedor) => {
+        this.fornecedores = listaFornecedor;
+      },
+      error: (e) => {
+        this.tratarErroCarregarLookup(e, this.nomeControlFornecedor);
+      },
+    });
+  }
+
+  private setLookupVacina() {
+    this.subscription = this.serviceVacina.listar().subscribe({
+      next: (listaVacina) => {
+        this.vacinas = listaVacina;
+      },
+      error: (e) => {
+        this.tratarErroCarregarLookup(e, this.nomeControlVacina);
+      },
+    });
+  }
+
+  preencherLookupVacina() {
+    this.subscription = this.serviceVacina.listar().subscribe({
+      next: (lista) => {
+        this.vacinas = lista;
+      },
+      error: (e) => {
+        this.tratarErroCarregarLookup(e, this.nomeControlFornecedor);
+      },
+    });
+  }
+
+  private setChangeFornecedorParaFiltrarValores() {
     this.fornecedoresFiltrados = this.form.controls[
-      'fornecedor'
+      this.nomeControlFornecedor
     ].valueChanges.pipe(
       startWith(''),
-      map((value) => {
-        let ehString = typeof value === 'string';
-        const nome = typeof value === 'string' ? value : value?.nome;
+      map((value) =>
+        this.filtrarPeloValor(
+          this.fornecedores,
+          value,
+          this.nomeControlFornecedor
+        )
+      )
+    );
+  }
 
-        if (
-          ehString &&
-          nome &&
-          nome != '' &&
-          this.fornecedores &&
-          this.fornecedores.length > 0
-        ) {
-          //https://stackoverflow.com/questions/45241103/patchvalue-with-emitevent-false-triggers-valuechanges-on-angular-4-formgrou
-          let user = this.fornecedores.find(
-            (user) => user.nome.toLowerCase() == nome.toLowerCase()
-          );
-          if (user) {
-            this.form.get('fornecedor')!.patchValue(user, { emitEvent: false });
-          }
-        }
-        return nome
-          ? this.filtrarValorLista(this.fornecedores, nome as string)
-          : this.fornecedores.slice();
-      })
+  setChangeVacinaParaFiltrarValores() {
+    this.vacinasFiltradas = this.form.controls[
+      this.nomeControlVacina
+    ].valueChanges.pipe(
+      startWith(''),
+      map((value) =>
+        this.filtrarPeloValor(this.vacinas, value, this.nomeControlVacina)
+      )
     );
   }
 }
