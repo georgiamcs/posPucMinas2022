ca63b853cb36292c000b10386f738a4f6ca3134b
Geórgia Maria <georgiamcs@gmail.com>
2023-02-28 12:53:31 -0300

    Backend - configurando o backend para levantar o frontend

--- projetoConclusaoPos/vacine/vacine-backend/src/setup/setup.js
+++ projetoConclusaoPos/vacine/vacine-backend/src/setup/setup.js
@@ -3,20 +3,33 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const PassportStrategy = require("../lib/autenticacao/passaport.lib");
+const path = require("path");
 
 module.exports = {
   init: (app) => {
-      //set up application middlewares
-      app.use(express.json());
-      app.use(cors());
-      app.use(
-        express.urlencoded({
-          extended: true,
-        })
+    //set up application middlewares
+    app.use(express.json());
+    app.use(cors());
+    app.use(
+      express.urlencoded({
+        extended: true,
+      })
+    );
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    // Apply strategy to passport
+    PassportStrategy.applyPassportStrategy(passport);
+
+    //Levantando o angular
+    app.use(
+      express.static(
+        path.join(__dirname, "../../../vacine-frontend/dist", "vacine")
+      )
+    );
+    app.get("/*", function (req, res) {
+      res.sendFile(
+        path.join(__dirname, "../../../vacine-frontend/dist", "vacine", "index.html")
       );
-      app.use(bodyParser.urlencoded({ extended: true }));
-      app.use(bodyParser.json());
-      // Apply strategy to passport
-      PassportStrategy.applyPassportStrategy(passport);
+    });
   },
 };
