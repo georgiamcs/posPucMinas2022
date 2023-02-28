3da79fc867f74883d42a845c8a5da261f534a170
Geórgia Maria <georgiamcs@gmail.com>
2023-02-28 12:43:27 -0300

    Alterando as configuracoes para publicacao no servidor

--- projetoConclusaoPos/vacine/vacine-frontend/src/app/environment.ts
+++ projetoConclusaoPos/vacine/vacine-frontend/src/app/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  //API_URL_BASE: 'http://localhost:3000/', // URL json-server
-  API_URL_BASE: 'http://localhost:3005/api/', // URL mongoose
+  API_URL_BASE: '/api/',
   GOOGLE_CLIENT_ID:
     '747953607133-up0551h3dcjlnkugvfgrmujtoiku0od4.apps.googleusercontent.com',
 };
